2a62d2ecbd0f398226c847cf3c75e7a254c274c7
Visjimenez <eliasjimenezmendez28@gmail.com>
2025-03-24 21:24:05 -0600

    CAMBIO 2

--- EventoGo/src/main/java/com/mycompany/eventogo/EventoGo.java
+++ EventoGo/src/main/java/com/mycompany/eventogo/EventoGo.java
@@ -10,7 +10,7 @@ public class EventoGo {
         cGestion gestion = new cGestion();
         int opcion;
 
-        do {
+        do { // soy Elías
             opcion = Integer.parseInt(JOptionPane.showInputDialog(
                 "Bienvenido(a) a EventosGo, seleccione una opción para continuar:\n" +
                 "1. Registrar usuario\n" +
